16b723c4b63aa7930d3ab46e63f04fea6b74be3d
Newton Duarte <newton_@hotmail.com>
2025-10-24 15:03:05 -0300

    fix: test comp lint and format errors

--- src/Comp.tsx
+++ src/Comp.tsx
@@ -1,14 +1,14 @@
-// src/components/TestComponent.tsx
-import React from 'react';
-
 const TestComponent = () => {
-  // This will show red underlines for formatting issues
-  const unusedVar = 'test'; // ESLint should warn about unused variable
+  const unusedVar = 'test';
+  const anotherUnusedVar = 'test2';
 
   return (
     <div>
       <h1>Test Component</h1>
       <p>This has inconsistent indentation</p>
+      <p>
+        {unusedVar}-{anotherUnusedVar}
+      </p>
     </div>
   );
 };
